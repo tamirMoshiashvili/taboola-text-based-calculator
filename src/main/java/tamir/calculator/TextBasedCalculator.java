019ff2a93a81f3e5b2d864fbb688fbfa303c2b59
Tamir <Tamir@DESKTOP-8R67EMV>
2024-04-25 01:49:00 +0300

    feat: read input by scanner until blank line is received

---
+++ src/main/java/tamir/calculator/TextBasedCalculator.java
@@ -0,0 +1,13 @@
+package tamir.calculator;
+
+import java.util.Scanner;
+
+public class TextBasedCalculator {
+
+	public static void runTextBasedCalculator(Scanner scanner) {
+		System.out.println("Enter expressions line by line, enter blank line for exit");
+		String line;
+		while (!(line = scanner.nextLine()).isBlank()) {
+		}
+	}
+}
