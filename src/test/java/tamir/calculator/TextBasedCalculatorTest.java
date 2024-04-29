619a2b67b3163f595e8d53871be12f3523bb51bb
Tamir <Tamir@DESKTOP-8R67EMV>
2024-04-30 02:55:13 +0300

    test: remove invalid test case Invalid expressions are allowed and does not throw exception

--- src/test/java/tamir/calculator/TextBasedCalculatorTest.java
+++ src/test/java/tamir/calculator/TextBasedCalculatorTest.java
@@ -1,12 +1,12 @@
 package tamir.calculator;
 
 import org.junit.jupiter.api.Test;
-import tamir.exception.UnknownVariableException;
 
 import java.util.Map;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static tamir.calculator.TextBasedCalculator.runTextBasedCalculator;
 
 class TextBasedCalculatorTest {
@@ -28,11 +28,6 @@ class TextBasedCalculatorTest {
 				"\n");
 		Map<String, Integer> variableToValue = runTextBasedCalculator(new Scanner(input));
 		assertEquals(Map.of("i", 37, "j", 1, "x", 6, "y", 35), variableToValue);
-	}
-
-	@Test
-	void whenUsingUnknownVariable_thenThrowsUnknownVariableException() {
-		assertThrows(UnknownVariableException.class, () -> runTextBasedCalculator(new Scanner("x = unknown\n\n")));
 	}
 
 	@Test
